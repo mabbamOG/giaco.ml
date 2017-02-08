f352e70c5eadfd69d975bfdfc2a14b1da97c1e51
@mabbamOG <mariobarbara@protonmail.ch>
2017-02-08 21:05:44 +0100

    fixes

--- denotational/staticenv/giaco.ml
+++ denotational/staticenv/giaco.ml
@@ -5,13 +5,14 @@
 
 (* internal language type system *)
 type value = Int of int | Bool of bool
-type variable = string
+(* type variable = string *) (* NOT TO BE USED UNLESS I  HAVE A CONSTRUCTOR *)
 
 (* external language parser through ocaml type constructors *)
 type expr =
 | Val of value
-| Var of variable
-| Plus of expr*expr
+(*| Var of variable*)
+| Var of string
+| Add of expr*expr
 (** external aliases to allow for the user input of types, basically a hack *)
 let int x= Val(Int(x))
 let bool b = Val(Bool(b))
@@ -23,18 +24,19 @@ let bool b = Val(Bool(b))
 (* internal implementation for the language, expects evaluated expressions *)
 let add = function
 | Int a, Int b -> Int (a+b)
-| _ -> failwith "plus operation not supported with these types"
+| _ -> failwith "add operation not supported with these types"
 
 (* interpreter for the language, environment is static and global *)
 (** type is (variable->value) -> expr -> value *)
 let rec eval env = function
 | Val a -> a
 | Var id -> env id
-| Plus (e1,e2)-> add (eval env e1,eval env e2)
+| Add (e1,e2)-> add (eval env e1,eval env e2)
 
 (***********************************************)
 (*****************TESTING***********************)
 (***********************************************)
+;;print_string "---------------commence phase of testing----------------"
 
 let test_env = function
 | "a" -> Int 3
@@ -42,6 +44,16 @@ let test_env = function
 | "ciao" -> Int 42
 | _ -> failwith "variable not bound"
 
-let _ = eval test_env (int 5)
-let _ = eval test_env (bool true)
-let _ = eval test_env (Plus(int 5, int 5))
+;;eval test_env (int 5)
+;;eval test_env (bool true)
+;;eval test_env (Add(int 5, int 5))
+;;print_string "---------------end phase of testing----------------"
+
+(***********************************************)
+(*****COMMANDDDDDDREFLECTION********************)
+(***********************************************)
+(*type typo = RInt of string*Int | *)
+(* 1. lex string into list of tokens *)
+(* 2. parse commands by splitting @ ; (?) and then read first word *)
+(* 3. profit *)
+(* 4. ???? *)
