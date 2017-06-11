30625008038a428c8ef5d1d0e889752a37e9b77d
@mabbamOG <mariobarbara@protonmail.ch>
2017-06-11 15:44:42 +0200

    partial, pdf missing

--- imperative/taint-analysis.ml
+++ imperative/taint-analysis.ml
@@ -19,10 +19,11 @@ let taint_analysis (src:prog) (p:tenv) (o:tstore) =
         in show_tenv p, show_tstore o
 
 (** TEST CASE **)
-let p = tenv' "xx" (Taint(Dirty)) emptytenv |> tenv' "yy" (Taint(Clean)) |> tenv' "clean" (TLoc(l))
+(*let p = tenv' "xx" (Taint(Dirty)) emptytenv |> tenv' "yy" (Taint(Clean)) |> tenv' "clean" (TLoc(l))
 let p,o = tnew' "clean" Clean p o
-let p,o = tnew' "dirty" Dirty p o
+let p,o = tnew' "dirty" Dirty p o*)
 
+(*
 let e = Equals(Plus(Val("x"),Val("y")),Int(6))
 let assign1 = CSeq(Assign("x", Val("dirty")), Assign("y", Val("clean")))
 let assign2 = CSeq(Assign("x", Val("clean")), Assign("y", Val("dirty")))
@@ -32,3 +33,4 @@ let if1 = CIfThen(e,CSeq(assign1,if2))
 let d = DSeq(New("x", Val("dirty")), New("y", Val("clean")))
 
 let result = taint_analysis (Prog(d,if1)) p o
+*)
